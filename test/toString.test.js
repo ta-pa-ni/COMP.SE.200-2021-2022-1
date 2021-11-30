f26d92ae36eea9d0a75054154cc143565864079b
ta-pa-ni <tapani.landen@gmail.com>
2021-11-30 16:27:16 +0200

    toString tests ready

--- test/toString.test.js
+++ test/toString.test.js
@@ -3,8 +3,60 @@ import toString from "../src/toString.js"
 
 const expect = chai.expect
 
+/* Equality classess with limit values:
+    Accepted:
+        Valid numbers
+        - decimal numbers
+            * MIN_VALUE
+        - negative integers
+            * MIN_SAFE_INTEGER
+        - positive integers
+            * MAX_SAFE_INTEGER
+        - zero
+       
+    Not accepted:
+        - No value
+    
+    Note: There is no need to test every possible limit since it really doesn't matter if
+    values beyond and below max safe are changed into strings because string representations 
+    of numbers are for showing them only, not for calclulations etc where too big numbers 
+    may cause some troubles.
+*/
+
 describe("toString", () => {
- it("change a number into a string representation of that number", () =>{
-     expect(toString(10)).to.equal("10")
- });
+    describe("basic values", () => {
+        it("change a positive integer into its string representation", () =>{
+            expect(toString(10)).to.equal("10")
+        });
+        it("change a negative integer into its string representation", () =>{
+            expect(toString(-210)).to.equal("-210")
+        });
+        it("change a negative decimal number into its string representation", () =>{
+            expect(toString(-210.0006)).to.equal("-210.0006")
+        });
+        it("change a positive decimal number into its string representation", () =>{
+            expect(toString(20.0006)).to.equal("20.0006")
+        });
+        it("change zero into its string representation", () =>{
+            expect(toString(0)).to.equal("0")
+        });
+    })
+
+    describe("accepted limit values", () => {
+        it("change MAX_SAFE_INTEGER into its string representation", () =>{
+            expect(toString(Number.MAX_SAFE_INTEGER)).to.equal("9007199254740991")
+        });
+        it("change MIN_SAFE_INTEGER into its string representation", () =>{
+            expect(toString(Number.MIN_SAFE_INTEGER)).to.equal("-9007199254740991")
+        });
+        it("change MIN_VALUE into its string representation", () =>{
+            expect(toString(Number.MIN_VALUE)).to.equal("5e-324")
+        });
+    })
+
+    describe("not accepted values", () => {
+        it("change nothing into string", () =>{
+            expect(toString()).to.equal("undefined")
+        });
+    })
 })
