d572ccd598c4388244806a692980e8c905797865
Milla Loimuvirta <milla.loimuvirta@gmail.com>
2021-12-03 13:49:03 +0200

    final isEmpty

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -7,22 +7,26 @@ const expect = chai.expect
 
 
 describe("isEmpty", () => {
- it("check if an object is empty", () =>{
-     expect(isEmpty({})).to.equal(true)
- });
- it("check if an object is not empty", () =>{
-    expect(isEmpty("text")).to.equal(false)
-});
-it("check if an object is null", () =>{
-    expect(isEmpty(null)).to.equal(true)
-});
-it("check if an empty sting is empty", () =>{
-    expect(isEmpty("")).to.equal(true)
-});
-it("check if an string is not empty", () =>{
-    expect(isEmpty(" ")).to.equal(false)
-});
-it("check if all is gone", () =>{
-    expect(isEmpty()).to.equal(true)
-});
+    describe("accepted values", () => {
+        it("check if an object is empty", () =>{
+            expect(isEmpty({})).to.equal(true)
+        });
+        it("check if an object is null", () =>{
+            expect(isEmpty(null)).to.equal(true)
+        });
+        it("check if an empty sting is empty", () =>{
+            expect(isEmpty("")).to.equal(true)
+        });
+        it("check if all is gone", () =>{
+            expect(isEmpty()).to.equal(true)
+        });
+    })
+    describe("not accepted values", () => {
+        it("check if an object is not empty", () =>{
+            expect(isEmpty("text")).to.equal(false)
+        });
+        it("check if an string is not empty", () =>{
+            expect(isEmpty(" ")).to.equal(false)
+        });
+    })
 })
