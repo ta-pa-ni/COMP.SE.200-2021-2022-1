9a40e4344caac353aacaf01b5416b67596091819
Milla Loimuvirta <milla.loimuvirta@gmail.com>
2021-12-03 12:39:24 +0200

    isempty

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -3,11 +3,17 @@ import isEmpty from "../src/isEmpty.js"
 
 const expect = chai.expect
 
+// check null tyhjä merkkijono   
+
+
 describe("isEmpty", () => {
  it("check if an object is empty", () =>{
      expect(isEmpty({})).to.equal(true)
  });
- it("check if an object is empty", () =>{
+ it("check if an object is not empty", () =>{
     expect(isEmpty("text")).to.equal(false)
+});
+it("check if an object is null", () =>{
+    expect(isEmpty(null)).to.equal(true)
 });
 })
