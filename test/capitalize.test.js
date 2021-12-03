f4e1b589d23f4e2b58c7e71a8f5d7f3527f877e1
ta-pa-ni <tapani.landen@gmail.com>
2021-12-03 15:38:19 +0200

    capitalize tests done

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,23 +1,64 @@
 import chai from 'chai'
 import capitalize from '../src/capitalize.js'
 
+/*
+  Equality classes with limit values
+
+  Accepted:
+  capitalize
+    - lower case string
+    - upper case string
+    - mixed string
+    - number
+    - boolean
+
+    Limit values:
+    - empty string
+  
+  Not accepted:
+    - null
+    - undefined
+
+*/
+
 const expect = chai.expect
 var should = chai.should()
 
-describe("capitalize different string parameters", () => {
-  it("to capitalize lowercase string", () =>{
-      expect(capitalize('lowercase')).to.equal('Lowercase')
-  });
-  it("to capitalize uppercase string", () =>{
-    expect(capitalize('UPPERCASE')).to.equal('Uppercase')
-  });
-})
+describe("capitalize", () => {
 
-describe("capitalize different non-string parameters", () => {
-  it("to return number as a string", () =>{
-    expect(capitalize(123)).to.equal('123')
-  });
-  it("to return boolean as capitalized string", () =>{
-    expect(capitalize(true)).to.equal('True')
-  });
+  describe("accepted strings", () => {
+    it("capitalize lowercase string", () =>{
+        expect(capitalize("this is very low.")).to.equal("This is very low.")
+    })
+    it("capitalize uppercase string", () =>{
+      expect(capitalize("THIS IS BIG!")).to.equal("This is big!")
+    })
+  })
+
+  describe("accepted limit values", () => {
+    it("capitalize lowercase string", () =>{
+        expect(capitalize("")).to.equal("")
+    })
+  })
+
+  describe("Not strings", () => {
+    it("capitalize number", () =>{
+      expect(capitalize(-0.10)).to.equal("-0.1")
+    })
+    it("capitalize boolean", () =>{
+      expect(capitalize(false)).to.equal("False")
+    })
+  })
+
+  describe("Not accepted values", () => {
+    it("try to capitalize empty", () =>{
+      expect(() =>capitalize()).to.throw(SyntaxError)
+    })
+    it("try to capitalize null", () =>{
+      expect(() =>capitalize(null)).to.throw(SyntaxError)
+    })
+    it("try to capitalize undefined", () =>{
+      expect(() =>capitalize(undefined)).to.throw(SyntaxError)
+    })
+  })
 })
