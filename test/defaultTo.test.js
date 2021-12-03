709928668b763586d3a4015bd44568187c479a04
Milla Loimuvirta <milla.loimuvirta@gmail.com>
2021-12-03 14:28:21 +0200

    defaultTo tests

--- test/defaultTo.test.js
+++ test/defaultTo.test.js
@@ -4,7 +4,52 @@ import defaultTo from "../src/defaultTo.js"
 const expect = chai.expect
 
 describe("defaultTo", () => {
- it("return default value when given undefined", () =>{
-     expect(defaultTo(undefined, 10)).to.equal(10)
- });
+    describe("setting default values to text fields", () => {
+        it("return default value when given undefined", () =>{
+            expect(defaultTo(undefined, "")).to.equal("")
+        });
+        it("return default value when given null", () =>{
+            expect(defaultTo(null, "")).to.equal("")
+        });
+        it("return default value when given Nan", () =>{
+            expect(defaultTo(NaN, "")).to.equal("")
+        });
+    })
+    describe("setting values to text fields", () => {
+        it("return correct value", () =>{
+            expect(defaultTo("correct value", "")).to.equal("correct value")
+        });
+    })
+    describe("setting default values to boolean", () => {
+        it("return default value when given undefined", () =>{
+            expect(defaultTo(undefined, false)).to.equal(false)
+        });
+        it("return default value when given null", () =>{
+            expect(defaultTo(null, false)).to.equal(false)
+        });
+        it("return default value when given Nan", () =>{
+            expect(defaultTo(NaN, false)).to.equal(false)
+        });
+    })
+    describe("setting values to boolean fields", () => {
+        it("return correct value", () =>{
+            expect(defaultTo(true, false)).to.equal(true)
+        });
+    })
+    describe("setting default values to number fields", () => {
+        it("return default value when given undefined", () =>{
+            expect(defaultTo(undefined, 0)).to.equal(0)
+        });
+        it("return default value when given null", () =>{
+            expect(defaultTo(null, 0)).to.equal(0)
+        });
+        it("return default value when given Nan", () =>{
+            expect(defaultTo(NaN, 0)).to.equal(0)
+        });
+    })
+    describe("setting values to number fields", () => {
+        it("return correct value", () =>{
+            expect(defaultTo(3, 0)).to.equal(3)
+        });
+    })
 })
