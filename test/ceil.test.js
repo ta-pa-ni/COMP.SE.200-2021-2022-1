ece16f36a7633d452ab9c9dc02dc435724d67d90
Milla Loimuvirta <milla.loimuvirta@gmail.com>
2021-12-03 15:03:23 +0200

    hups taas

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -5,13 +5,14 @@ const expect = chai.expect
 
 describe("ceil", () => {
     describe("ceil up", () => {
- it("take a ceil function out of a number 7,4 to 8", () =>{
-     expect(ceil(7.4)).to.equal(8)
- });
- it("take a ceil function out of a number 3,006 with 2 percision", () =>{
-    expect(ceil(3.006, 2)).to.equal(3.01)
-});
- it("take a ceil function out of a number 7 with -1 precision", () =>{
-    expect(ceil(7,-1)).to.equal(10)
-});
+        it("take a ceil function out of a number 7,4 to 8", () =>{
+            expect(ceil(7.4)).to.equal(8)
+        });
+        it("take a ceil function out of a number 3,006 with 2 percision", () =>{
+            expect(ceil(3.006, 2)).to.equal(3.01)
+        });
+        it("take a ceil function out of a number 7 with -1 precision", () =>{
+            expect(ceil(7,-1)).to.equal(10)
+        });
+    })
 })
