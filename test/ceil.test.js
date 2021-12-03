9b0809380c2e2265832204245029d91bc84437a2
Milla Loimuvirta <milla.loimuvirta@gmail.com>
2021-12-03 15:00:40 +0200

    hups

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -9,7 +9,7 @@ describe("ceil", () => {
      expect(ceil(7.4)).to.equal(8)
  });
  it("take a ceil function out of a number 3,006 with 2 percision", () =>{
-    expect(ceil(3.006, 2)).to.equal(3,01)
+    expect(ceil(3.006, 2)).to.equal(3.01)
 });
  it("take a ceil function out of a number 7 with -1 precision", () =>{
     expect(ceil(7,-1)).to.equal(10)
